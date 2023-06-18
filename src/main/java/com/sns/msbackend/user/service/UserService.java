590e9d86e9143875b0896ac3e523f5fe0aa9e0fe
Septian <septian.solihin01@gmail.com>
2023-06-18 11:54:30 +0700

    create user controller and service

---
+++ src/main/java/com/sns/msbackend/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sns.msbackend.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public void getUser(){
+        System.out.println("User is exists");
+    }
+}
